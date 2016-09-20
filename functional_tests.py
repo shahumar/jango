e51d179268dbad64415db21f8ab75e09c48464e9
shah-ebryx <shah.umar@ebryx.com>
2016-09-20 10:39:10 +0500

    Test for template rendering and model

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,12 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
+
 	def test_can_start_a_list_and_retrieve_it_latter(self):
 		#edita heard about cool new online to do app she goes
 		#to check out its homepage
@@ -35,23 +41,23 @@ class NewVisitorTest(unittest.TestCase):
 		# When she hits enter, the page updates, and now the page lists
 		# "1: Buy peacock feathers" as an item in a to-do list table
 		inputbox.send_keys(Keys.ENTER)
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows),
-			"To-Do item not appear in the list"
-		)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
-		self.fail('Tests finished')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+
 		# The page updates again, and now shows both items on her list 	 	
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
 		# Edith wonders whether the site will remember her list. Then she sees
 		# that the site has generated a unique URL for her -- there is some
 		# explanatory text to that effect.
 		# She visits that URL - her to-do list is still there.
 		# Satisfied, she goes back to sleep
-
+		self.fail('Tests finished')
 
 if __name__ == '__main__':
 	unittest.main()
--- superlist/lists/tests.py
+++ superlist/lists/tests.py
@@ -1,9 +1,10 @@
 from django.core.urlresolvers import resolve
-from django.template.loader import render_to_string
+from django.shortcuts import render
 from django.test import TestCase
 from django.http import HttpRequest
 
 from lists.views import home_page
+from lists.models import Item
 
 class HomePageTest(TestCase):
 
@@ -14,6 +15,32 @@ class HomePageTest(TestCase):
 	def test_home_page_return_correct_html(self):
 		request = HttpRequest()
 		response = home_page(request)
-		expected_html = render_to_string('home.html')
+		self.assertIn('To-Do lists', response.content.decode())
 
-		self.assertEqual(response.content.decode(), expected_html)
+	def test_home_can_save_post_request(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+		response = home_page(request)
+		self.assertIn('A new list item', response.content.decode())
+		
+
+
+class ItemModelTest(TestCase):
+
+	def test_saving_and_retrieving_items(self):
+		first_item = Item()
+		first_item.text = 'The first ever list item'
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.save()
+
+		saved_item = Item.objects.all()
+		self.assertEqual(saved_item.count(), 2)
+
+		first_saved_item = saved_item[0]
+		second_saved_item = saved_item[1]
+		self.assertEqual(first_saved_item.text, 'The first ever list item')
+		self.assertEqual(second_saved_item.text, 'Item the second')
