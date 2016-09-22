e9f17495a89eea2866d5118002c9cc90ae4771f5
shah-ebryx <shah.umar@ebryx.com>
2016-09-22 11:17:42 +0500

    use bootstrap to improve layouts

--- superlist/functional_tests/tests.py
+++ superlist/functional_tests/tests.py
@@ -1,9 +1,10 @@
 from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 	"""docstring for NewVisitorTest"""
 
 	def setUp(self):
@@ -84,3 +85,17 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertIn('Buy milk', page_text)
 		# Satisfied, they both go back to sleep
 		self.fail('Tests finished')
+
+	def test_layout_and_styling(self):
+		#edith goes to the home page
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+		#she notices the input box i nicely centered
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=5
+		)
